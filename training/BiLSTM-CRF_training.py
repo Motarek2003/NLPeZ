7f1935bbe04a3610fc3576a576973630fae7747c
Abd El-Rahman Hayder <lboodyde02@gmail.com>
2025-12-14 17:14:09 +0200

    g

--- training/BiLSTM-CRF_training.py
+++ training/BiLSTM-CRF_training.py
@@ -5,8 +5,6 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
